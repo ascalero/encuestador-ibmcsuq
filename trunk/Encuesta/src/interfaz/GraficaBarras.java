db6e07f25e94467ac3a4fd04f2c8d714f293e3f6
chiscoice@gmail.com <chiscoice@gmail.com@5751fc82-5ba4-c7e0-ff0a-bb5553a5f9bc>
2012-07-15 02:36:18 +0000

    Grafica de Barras

---
+++ trunk/Encuesta/src/interfaz/GraficaBarras.java
@@ -0,0 +1,48 @@
+package interfaz;
+
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import javax.swing.JPanel;
+import org.jfree.chart.*;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Shadow
+ */
+public class GraficaBarras extends JPanelConFondo{
+    
+    private DefaultCategoryDataset dataset;
+    private String nameProy;
+    
+    public GraficaBarras(String nameProy) {
+        dataset = new DefaultCategoryDataset(); 
+        this.nameProy=nameProy;
+    }
+    
+    public void setDatos(ArrayList<String> nameEnc,int[] answer,String name){
+        if (nameEnc!=null){
+            for(int i=0;i< nameEnc.size();i++){
+                dataset.addValue(answer[i], name, nameEnc.get(i));
+            }
+            JFreeChart graficoBarras = ChartFactory.createBarChart( 
+                nameProy,        //Nombre del Proyecto 
+                "Usuarios",      //etiqueta Eje horizontal 
+                "Opciones",      //etiqueta Eje vertical 
+                dataset,                   //datos 
+                PlotOrientation.VERTICAL,  //orientación 
+                true,                      //incluir leyendas 
+                true,                      //mostrar tooltips 
+                true);
+        
+        this.setImagen(graficoBarras.createBufferedImage(640,480));
+        }
+    }
+}
