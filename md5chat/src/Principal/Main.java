c75f5d26e07d9c1360cd973a0c70ba084b356760
chiscoice <chiscoice@gmail.com>
2012-11-01 17:48:59 +0000

    Test de Conexión

---
+++ md5chat/src/Principal/Main.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Principal;
+
+/**
+ *
+ * @author Shadow
+ */
+public class Main {
+    public static void main(String args[]){
+        System.out.println("Test de Conexión");
+    }
+}
