58b75c0864ba6adb983d20afac9ee0819ee7554e
mjvikk@gmail.com <mjvikk@gmail.com@5751fc82-5ba4-c7e0-ff0a-bb5553a5f9bc>
2013-04-13 04:37:55 +0000

    prueba commit

---
+++ trunk/AplicacionPrueba/src/aplicacionprueba/AplicacionPrueba.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacionprueba;
+
+/**
+ *
+ * @author vikko
+ */
+public class AplicacionPrueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
